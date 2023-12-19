fbe1448b54ec0d58a9650ebb9e8473edca2888c7
Zeeshan Anwar <mranwar08@gmail.com>
2023-12-19 11:29:29 +0000

    Dark Mode

--- index.php
+++ index.php
@@ -14,10 +14,10 @@ include("db_config.php");           // Configure Database
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Health Advice Group</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">         <!-- Bootstrap CSS -->
 </head>
-<body class="bg-light">
+<body class="bg-dark">
 
     <div class="container mt-5">
         <div class="row justify-content-center">
