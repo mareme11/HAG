6f9daef612486a66f9716d416bbb686fa2fb29df
anwarcomputing <152861780+anwarcomputing@users.noreply.github.com>
2023-12-19 10:39:40 +0000

    Moved Comments

--- index.php
+++ index.php
@@ -1,20 +1,12 @@
 <?php
 
-// Start the session
-session_start();
-
-if (isset($_GET['logout'])) {
-    // Destroy the session
+session_start();                    // Start the session
+if (isset($_GET['logout'])) {       // Destroy the session
     session_destroy();
-    
-    // Redirect to the login page
-    header('Location: .');
+    header('Location: .');           // Redirect to the login page
     exit;
 }
-
-// Configure Database
-include("db_config.php");
-
+include("db_config.php");           // Configure Database
 ?>
 
 <!DOCTYPE html>
@@ -23,33 +15,23 @@ include("db_config.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-
-    <!-- Bootstrap CSS -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">         <!-- Bootstrap CSS -->
 </head>
 <body class="bg-light">
 
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-6">
-                <?php
-               
-
-                // Check if the user is already logged in
-                if (isset($_SESSION['username'])) {
+                <?php              
+                if (isset($_SESSION['username'])) {             // Check if the user is already logged in
                     echo '<div class="alert alert-success" role="alert">Welcome back, ' . $_SESSION['username'] . '! <a href="?logout=1">Logout</a></div>';
-                    
-                } else {
-                    // If not logged in, check if the form is submitted
-                    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                        // Validate the login credentials (you may replace this with a database check)
+                } else {                                         // If not logged in, check if the form is submitted
+                    if ($_SERVER['REQUEST_METHOD'] === 'POST') { // Validate the login credentials (you may replace this with a database check)     
                         $escaped_username = $conn->real_escape_string($_POST['username']);
-                        $escaped_password = $conn->real_escape_string($_POST['password']);
-                        // Query to check login credentials
+                        $escaped_password = $conn->real_escape_string($_POST['password']);// Query to check login credentials
                         $sql = "SELECT * FROM users WHERE username='$escaped_username' AND password='$escaped_password'";
                         $result = $conn->query($sql);
-                        if ($result->num_rows > 0) {
-                            // Set the session variable
+                        if ($result->num_rows > 0) {              // Set the session variable     
                             $_SESSION['username'] = $escaped_username;
                             echo '<div class="alert alert-success" role="alert">Login successful. Welcome, ' . $escaped_username . '! <a href="?logout=1">Logout</a></div>';
                         } else {
@@ -57,8 +39,7 @@ include("db_config.php");
                             include ("loginform.html");
                         }
                     } else {
-                        // Display the login form
-                        include ("loginform.html");
+                        include ("loginform.html");// Display the login form
                     }
                 }
                 ?>
