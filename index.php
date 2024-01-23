1f8471c6bb25a0369bfe086daae24d14b82e0ac3
mareme11 <154320153+mareme11@users.noreply.github.com>
2024-01-23 10:03:05 +0000

    added image

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ include("db_config.php");           // Configure Database
 <body class="bg-dark">
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Health Advice Group</a>
+        <a class="navbar-brand" href="index.php"><img src="logo.png"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
